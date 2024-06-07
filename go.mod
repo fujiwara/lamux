e7f224e03ed3f87f403c194218c0ca17ec867d2c
fujiwara <fujiwara.shunichiro@gmail.com>
2024-06-08 00:34:19 +0900

    impl

--- go.mod
+++ go.mod
@@ -1,3 +1,30 @@
 module github.com/fujiwara/lamux
 
 go 1.21
+
+require (
+	github.com/alecthomas/kong v0.9.0
+	github.com/aws/aws-sdk-go-v2 v1.27.1
+	github.com/aws/aws-sdk-go-v2/config v1.27.17
+	github.com/aws/aws-sdk-go-v2/service/lambda v1.54.5
+	github.com/fujiwara/ridge v0.9.2
+)
+
+require (
+	github.com/aws/aws-lambda-go v1.46.0 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.2 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.17 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.8 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.8 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.20.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.24.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.28.11 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pires/go-proxyproto v0.7.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,131 @@ package lamux
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/alecthomas/kong"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"github.com/fujiwara/ridge"
 )
 
+type Config struct {
+	Port            int           `help:"Port to listen on" default:"8080" env:"LAMUX_PORT" name:"port"`
+	FunctionName    string        `help:"Name of the Lambda function to proxy" required:"" env:"LAMUX_FUNCTION_NAME" name:"function-name"`
+	DomainSuffix    string        `help:"Domain suffix to accept requests for" required:"" env:"LAMUX_DOMAIN_SUFFIX" name:"domain-suffix"`
+	UpstreamTimeout time.Duration `help:"Timeout for upstream requests" default:"30s" env:"LAMUX_UPSTREAM_TIMEOUT" name:"upstream-timeout"`
+}
+
+type Lamux struct {
+	Config       *Config
+	awsCfg       aws.Config
+	lambdaClient *lambda.Client
+}
+
+func NewLamux(cfg *Config) (*Lamux, error) {
+	awsCfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("failed to load AWS config: %w", err)
+	}
+	return &Lamux{
+		Config:       cfg,
+		awsCfg:       awsCfg,
+		lambdaClient: lambda.NewFromConfig(awsCfg),
+	}, nil
+}
+
+var aliasRegexp = regexp.MustCompile(`^[a-zA-Z0-9-]+$`)
+
 func Run(ctx context.Context) error {
-	fmt.Println("lamux!")
+	cfg := &Config{}
+	kong.Parse(cfg)
+
+	l, err := NewLamux(cfg)
+	if err != nil {
+		return err
+	}
+	var mux = http.NewServeMux()
+	mux.HandleFunc("/", l.handleProxy)
+
+	addr := fmt.Sprintf(":%d", cfg.Port)
+	ridge.Run(addr, "/", mux)
 	return nil
+}
+
+func (l *Lamux) handleProxy(w http.ResponseWriter, r *http.Request) {
+	alias, err := extractAlias(r, l.Config.DomainSuffix)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	payload, err := ridge.ToRequestV2(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	b, err := json.Marshal(payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	input := &lambda.InvokeInput{
+		FunctionName: &l.Config.FunctionName,
+		Payload:      b,
+		Qualifier:    aws.String(alias),
+	}
+	ctx, cancel := context.WithTimeout(r.Context(), l.Config.UpstreamTimeout)
+	defer cancel()
+	start := time.Now()
+	resp, err := l.lambdaClient.Invoke(ctx, input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	elapsed := time.Since(start)
+	slog.Info("upstream", "duration", elapsed, "alias", alias, "status", resp.StatusCode)
+	if resp.FunctionError != nil {
+		http.Error(w, *resp.FunctionError, http.StatusInternalServerError)
+		return
+	}
+	var res ridge.Response
+	if err := json.Unmarshal(resp.Payload, &res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if n, err := res.WriteTo(w); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		slog.Info("response", "method", r.Method, "url", r.URL.String(), "status", res.StatusCode, "bytes", n)
+	}
+}
+
+func extractAlias(r *http.Request, suffix string) (string, error) {
+	var host string
+	var err error
+	if host = r.Header.Get("X-Forwarded-Host"); host == "" {
+		host = r.Host
+	}
+	host, _, err = net.SplitHostPort(host)
+	if err != nil {
+		return "", fmt.Errorf("invalid host")
+	}
+	slog.Info("extractAlias", "host", host, "suffix", suffix)
+	if !strings.HasSuffix(host, suffix) {
+		return "", fmt.Errorf("invalid domain suffix")
+	}
+	alias := strings.TrimSuffix(host, "."+suffix)
+	if !aliasRegexp.MatchString(alias) {
+		return "", fmt.Errorf("invalid alias")
+	}
+	return alias, nil
 }
